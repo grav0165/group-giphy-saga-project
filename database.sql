9c11622a8787817f4c458aa1ae8906217949c33b
Aubrey Graves <grav0165@gmail.com>
2023-07-20 11:46:55 -0500

    Created favorites database

--- database.sql
+++ database.sql
@@ -1,5 +1,10 @@
 CREATE DATABASE "giphy_search_favorites";
 
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "link" VARCHAR (500) NOT NULL,
+    "catagory_id" INT
+)
 -- You'll need a table for storing each giphy image favorite
 -- Each favorite image can be assigned 1 of the following categories as a Foreign Key
 
--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -8,6 +8,8 @@ router.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
+// grab image
+
 // add a new favorite
 router.post('/', (req, res) => {
   res.sendStatus(200);
