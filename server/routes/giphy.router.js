32185624cd7a992ab62d744667626de9fb16a4cd
Jonathan Viegut <viegutj@gmail.com>
2023-07-20 16:04:40 -0500

    getting back GIFs from the API

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -4,8 +4,10 @@ const axios = require('axios');
 
 const giphyRouter = express.Router()
 
-giphyRouter.get('/', (req, res) => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}`)
+giphyRouter.get('/:search', (req, res) => {
+    let searchQuery = req.params.search
+    console.log('req.body: ', searchQuery);
+    axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${searchQuery}&limit=4`)
     .then(response => {
         res.send(response.data)
     }).catch(err => {
